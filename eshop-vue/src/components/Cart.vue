93115b53ab6b28445a5ade8929f0d8b5663dbb97
Dmirty <dmitry.ml@mail.ru>
2021-06-08 00:42:10 +0300

    dz7_

---
+++ eshop-vue/src/components/Cart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div v-show="isVisibleCart" style="text-align: center;" class="cart">
+    Корзина:
+    <div class="cart-list">
+    <div v-for="item in cartGoods " :key="item.id_product" class='goods-item'>
+        <div class="text-center">
+          <div class="card-body">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>{{ item.product_name }}</li>
+            </ul>
+            <h1 class="card-title pricing-card-title">{{ item.price }}</h1>
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary" @click="onClick(item)">Удалить</button>
+          </div>
+        </div>
+     </div>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick(item) {
+      this.$emit('delete-from-cart', item);
+    }
+  },
+  props: {
+    isVisibleCart: {
+      type: Boolean,
+      default: false,
+    },
+    cartGoods: {
+      type: Array,
+      default: () => ([]),
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cart-list {
+  display: flex;
+  flex-flow: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.goods-item {
+  width: 200px;
+  height: 300px;
+  padding: 5px;
+  margin: 10px;
+  box-shadow: 0px 0px 8px 2px rgba(34, 60, 80, 0.2);
+}
+</style>
---
+++ eshop-vue/src/components/GoodsList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div style="text-align: center;">Товары:
+      <div class="goods-list">
+        <div v-for="item in goods" :key="item.id_product" class='goods-item'>
+          <div class="text-center">
+            <div class="card-body">
+              <ul class="list-unstyled mt-3 mb-4">
+                <li>{{ item.product_name }}</li>
+              </ul>
+                <h1 class="card-title pricing-card-title">{{ item.price }}</h1>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary" @click="onClick(item)">Добавить</button>
+            </div>
+          </div>
+        </div>
+     </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    goods: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  methods: {
+    onClick(item) {
+      this.$emit('add-to-cart', item);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.goods-list {
+  display: flex;
+  flex-flow: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.goods-item {
+  width: 200px;
+  height: 300px;
+  padding: 5px;
+  margin: 10px;
+  box-shadow: 0px 0px 8px 2px rgba(34, 60, 80, 0.2);
+}
+</style>
